5437e05d7e0590a019627f3f9edf4cab3fc669e2
fasasimayokun <maykay.fash@gmail.com>
2024-04-09 21:04:26 +0100

    my final commit

--- Users/urls.py
+++ Users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('register/', views.user_registration, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
-    path('welcome/', views.WelcomeView.as_view(), name='welcome')
+    path('', views.WelcomeView.as_view(), name='welcome')
 ]
